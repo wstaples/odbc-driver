746d863ecaa351c505a729476d58cb074fc9f975
Wesley Staples <wstaples@geiger.com>
2014-09-17 15:32:14 -0400

    removed the username and password from the DSN which was causing it not to work with IBM iseries odbc

--- src/Ccovey/ODBCDriver/ODBCDriverConnector.php
+++ src/Ccovey/ODBCDriver/ODBCDriverConnector.php
@@ -20,9 +20,7 @@ class ODBCDriverConnector extends Connector implements ConnectorInterface
 
 	protected function getDsn(array $config) {
         extract($config);
-
-        $dsn = "odbc:{$dsn}; {$username}; {$password};";
-
+		$dsn = "odbc:{$dsn}";
         return $dsn;
     }
 }
